ce90ffd219617cbc25d0f57b8517d69343185f57
Simone de Abreu <siabreu@gmail.com>
2023-05-18 12:45:26 -0300

    modify template welcome

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario 'Mostra a mensagem de Bem-Vindo' do
-    visit '/'
+    # visit '/'
+    visit root_path
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario 'Verificar o link de cadastro de clientes' do
+    visit root_path
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
